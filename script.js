805229cfccf42e05d457df373d602b586f2bda85
Husaam NT <husnaeem@gmail.com>
2023-03-09 00:54:48 +0000

    added data.list from api info

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-// const currentDay = document.getElementById('currentDay')
-// const cDTemp = document.getElementById('cDTemp')
-// const cDWind = document.getElementById('cDWind')
-// const cDHumidity = document.getElementById('cDHumidity')
-// const searchInput = document.getElementById('searchInput')
-// const searchButton = document.getElementById('searchButton')
+const currentDay = document.getElementById('currentDay')
+const cDTemp = document.getElementById('cDTemp')
+const cDWind = document.getElementById('cDWind')
+const cDHumidity = document.getElementById('cDHumidity')
+const searchInput = document.getElementById('searchInput')
+const searchButton = document.getElementById('searchButton')
 
-// searchInput.addEventListener("input", function(){
-//     var inputValue = this.value;
-//     console.log(inputValue)
-// })
+
+searchButton.addEventListener("click", async function(event){
+    event.preventDefault();
+    var inputValue = searchInput.value;
+    const location = await locationRetrival(inputValue);
+    const { lat, lon } = location;
+    await weatherRetrival(lat, lon)
+})
 
 async function locationRetrival(inputLocation){
-    console.log("location")
     const response = await fetch( "http://api.openweathermap.org/geo/1.0/direct?q="+ inputLocation +"&appid=149603609ebf41d4d39209c578af7f8c")
-    console.log(response)
-    const lat = response.lat
-    const lon = response.lon
-    console.log(lat)
-    console.log(lon)    
+   const data = await response.json()
+    const lat = data[0].lat
+    const lon = data[0].lon  
     return { lat, lon }
 }
 
 
 async function weatherRetrival(lat, lon){
     console.log("weather")
-    const response = await fetch ("api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=149603609ebf41d4d39209c578af7f8c")
+    const response = await fetch ("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=149603609ebf41d4d39209c578af7f8c")
     console.log(response)
+    const data = await response.json()
+    console.log(data.list[3])
+    console.log(data.list[11])
+    console.log(data.list[19])
+    console.log(data.list[27])
+    console.log(data.list[35])
 }
 
 async function searchWeather(){
-    console.log("hello")
     const location = await locationRetrival("london");
     const { lat, lon } = location;
     await weatherRetrival(lat, lon)
 }
 
-searchWeather()
+//searchWeather()
+
+console.log()
